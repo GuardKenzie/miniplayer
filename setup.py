2c73f1e675e4ceed4e8fffd1576cc6cf64082605
Tristan Ferrua <tristanferrua@gmail.com>
2021-01-29 15:43:04 +0000

    Updated setup script for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name="miniplayer",
       version="1.0",
       description="An mpd client with album art and basic functionality written for use with the kitty terminal.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url="https://github.com/GuardKenzie/miniplayer",
       author="Tristan Ferrua",
       author_email="tristanferrua@gmail.com",
