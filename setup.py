97c3bb3a3771f14f4ec274dce7022128823a1bcd
Tristan Ferrua <tristanferrua@gmail.com>
2021-01-31 00:11:36 +0000

    Updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name="miniplayer",
-      version="1.0",
+      version="1.0.1",
       description="An mpd client with album art and basic functionality written for use with the kitty terminal.",
       long_description=long_description,
       long_description_content_type="text/markdown",
