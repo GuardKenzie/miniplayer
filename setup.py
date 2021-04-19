28059ed42d26999bef085afadf5df2887c911a14
GuardKenzie <tristanferrua@gmail.com>
2021-04-19 18:08:30 +0000

    Moved to PEP 518 compliant setup

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
-from setuptools import setup
+import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(name="miniplayer",
-      version="1.1.2",
-      description="An mpd client with album art and basic functionality.",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url="https://github.com/GuardKenzie/miniplayer",
-      author="Tristan Ferrua",
-      author_email="tristanferrua@gmail.com",
-      license="MIT",
-      scripts=["bin/miniplayer"],
-      install_requires=[
-          "python-mpd2",
-          "ffmpeg-python",
-          "pixcat",
-          "pillow",
-          "ueberzug"
-      ])
-
+if __name__ == '__main__':
+    setuptools.setup()
